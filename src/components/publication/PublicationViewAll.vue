30472dc972e598bf68fff026312d5d8749568495
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2022-03-29 02:03:38 +0300

    first commit

---
+++ src/components/publication/PublicationViewAll.vue
@@ -0,0 +1,17 @@
+<script setup>
+const message = "Tämä on testi."
+const loggedIn = true
+</script>
+
+<template>
+    <h1>PublicationViewAll OK!</h1>
+    <p>{{ message }}</p>
+    <h1 v-if="loggedIn">LoggedIn on TRUE!</h1>
+    <h1 v-else>LoggedIn on false :(</h1>
+</template>
+
+<style>
+    body {
+        margin: 30px;
+    }
+</style>
