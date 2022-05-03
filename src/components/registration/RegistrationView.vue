57229c491881b4a04afd4dcbcca804b6624e327c
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2022-05-04 02:59:51 +0300

    salasanan tarkistus tehty

--- src/components/registration/RegistrationView.vue
+++ src/components/registration/RegistrationView.vue
@@ -2,30 +2,72 @@
 import { reactive, computed } from "vue"
 import { useRouter } from "vue-router";
 
-const publicationData = reactive({
+const userData = reactive({
     name: "",
     password: "",
     email: ""
 })
 
+const rewrite = reactive({
+    password: ""
+})
+
 const router = useRouter()
+
+const passwordCheck = () => {
+    
+    let hasNumber = false
+    let hasUpper = false
+    let hasSpecial = false
+    let isLongEnough = false
+    let rewriteMatch = false
+
+    // Tarkastetaan numerot
+    if (userData.password.match(/[0-9]/)) {
+        hasNumber = true
+    } 
+    // Tarkastetaan isot kirjaimet
+    if (userData.password.match(/[A-Z]/)) {
+        hasUpper = true
+    } 
+    // Tarkastetaan erikoismerkit
+    if (userData.password.match(/[!@#$%^&*(),.?":{}|<>]/)) {
+        hasSpecial = true
+    } 
+    // Tarkastetaan salasanan pituus
+    if (userData.password.length >= 8){
+        isLongEnough = true
+    }
+    // Tarkastetaan salasanan uudelleenkirjoitus
+    if (userData.password == rewrite.password){
+        rewriteMatch = true
+    }
+
+    if (hasNumber && hasUpper && hasSpecial && isLongEnough && rewriteMatch){
+        return true
+    }
+
+    return false
+}
+
 
 // Palauttaa objektin
 const validationObject = computed(() => {
 
     // Nämä arvot ovat true tai false
     const nameValidation = userData.name.length > 2
-    const passwordValidation = userData.password.length < 1000
-    const emailValidation = userData.email.includes("@")
+    const passwordValidation = passwordCheck()
+    const emailValidation = userData.email.includes('@')
 
     return {
         // Jos true niin "OK", muulloin toinen vaihtoehto
-        nameValidation: titleValidation ? "OK" : "Nimen täytyy olla ainakin kolme merkkiä pitkä",
-        passwordValidation: descriptionValidation ? "OK" : "Kuvauksen teksti on liian pitkä",
-        emailValidation: urlValidation ? "OK" : "Vain https osoitteet ovat sallittu",
+        nameValidation: nameValidation ? "OK" : "Nimen täytyy olla ainakin kolme merkkiä pitkä",
+        passwordValidation: passwordValidation ? "OK" : "Salasanassa täytyy olla vähintään yksi iso kirjain, numero ja erikoismerkki. Salasanan täytyy olla vähintään kahdeksan merkkiä pitkä.",
+        emailValidation: emailValidation ? "OK" : "Vain e-mail osoitteet ovat sallittu",
         isAllValid: nameValidation && passwordValidation && emailValidation
     }
 })
+
 const createNewUser = async () => {
 
     if(!validationObject.value.isAllValid) return
@@ -46,18 +88,21 @@ const createNewUser = async () => {
 <template>
     <h2>Luo uusi käyttäjä</h2>
     <label><b>Käyttäjätunnus</b></label>
-    
+    <small>{{ validationObject.nameValidation }}</small>
     <input v-model="userData.name" type="text">
     <br>
     <label><b>Salasana</b></label>
-
-    <input v-model="userData.password" type="text">
+    <small>{{ validationObject.passwordValidation }}</small>
+    <input v-model="userData.password" type="password">
+    <br>
+    <label><b>Kirjoita salasana uudelleen</b></label>
+    <input v-model="rewrite.password" type="password">
     <br>
     <label><b>Sähköposti</b></label>
-    
+    <small>{{ validationObject.emailValidation }}</small>
     <input v-model="userData.email" type="text">
     <br>
-    <button :disabled="!validationObject.isAllValid" @click="createNewPublication">Lähetä</button>  
+    <button :disabled="!validationObject.isAllValid" @click="createNewUser">Lähetä</button>  
 </template>
 
 <style scoped>
--- src/services/authService.js
+++ src/services/authService.js
@@ -5,8 +5,11 @@ import { globalState } from "../store"
 export const authService = {
     async useLogin(payload){   // Tässä payloadina on credentials joka lähetetään back-endin osoitteeseen
                                // https://vara.onrender.com/api/login
+
+        // back-end tarkastus
         const {data, error} = await useApi('/login').post(payload).json()
 
+        // jos kaikki ok, niin globalState saa tokenin back-endiltä
         if(!error.value && data.value){
             globalState.accessToken = data.value.access_token
         }
